3f9ec260274f4ddce4c2c9299e5b3f3eda26210d
Zaur <askerov.zaur@gmail.com>
2023-10-20 19:41:41 +0400

    Initial Commit

---
+++ Expenses/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Expenses
+//
+//  Created by Zaur Askerov on 20.10.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
